733713eb001cf4580a04227c65849c962f844ce4
shelldonryan <shelldon.boy15@gmail.com>
2023-10-10 19:51:01 -0300

    Atividade de Interface com Zoo

---
+++ sistema-zoologico2/src/br/com/cz/zoologico/Model/Mamifero.java
@@ -0,0 +1,13 @@
+package br.com.cz.zoologico.Model;
+
+public abstract class Mamifero extends Animal{
+
+    public void nadar(){
+        System.out.println("Nadando");
+    }
+
+    public void escalar(){
+        System.out.println("Escalando");
+    }
+
+}
